f45af24315531f592c69b889892b9217ad5a683b
Mahialam Rahat <mahialam408@gmail.com>
2026-02-16 11:27:33 +0600

    hero section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Hero from './Hero/Hero';
 
 const Home = () => {
     return (
-        <div>
-            This is home
-        </div>
+        <>
+            <Hero />
+        </>
     );
 };
 
